a5c88514cb2fd494a2fabbc68ccccd212dcdd3df
juwon0295 <sungjuwon1@gmail.com>
2026-04-07 21:35:04 +0900

    버튼 이동 구현

---
+++ Assets/02. Scripts/ReturnButton.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class ReturnButton : MonoBehaviour
+{
+    public float interactDistance = 3f; // 상호작용 가능한 거리
+    public TextMeshProUGUI interactText; // 안내 텍스트 연결할 변수
+    private Transform player;
+
+    void Start()
+    {
+        player = GameObject.Find("Player").transform;
+        interactText.gameObject.SetActive(false); // 시작할 때 텍스트 숨기기
+    }
+
+    void Update()
+    {
+        float distance = Vector3.Distance(player.position, transform.position);
+
+        if (distance < interactDistance)
+        {
+            interactText.gameObject.SetActive(true); // 가까이 가면 텍스트 표시
+
+            if (Input.GetKeyDown(KeyCode.E)) // E키를 눌러서 씬 이동
+            {
+                SceneManager.LoadScene("CharacterMoveScene");
+            }
+        }
+        else
+        {
+            interactText.gameObject.SetActive(false); // 멀어지면 텍스트 숨기기
+        }
+    }
+}
